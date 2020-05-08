f993bd8bcb8cee889f7e1010571c54020ece5a89
Roman Yavnikov <45608740+Romazes@users.noreply.github.com>
2020-05-08 12:44:22 +0300

    3.7. Book Index Get Handler

---
+++ BookListRazor/BookListRazor/Pages/BookList/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class IndexModel : PageModel
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public IndexModel(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IEnumerable<Book> Books { get; set; }
+
+        public async Task OnGet()
+        {
+            Books = await _appDbContext.Book.ToListAsync();
+        }
+    }
+}
