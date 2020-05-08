251df7cc8fdaaa4f3827d6e61823f9f54d301633
Roman Yavnikov <45608740+Romazes@users.noreply.github.com>
2020-05-08 23:53:33 +0300

    5.3. Add Category Controller and Get Action Method

---
+++ WebStore/WebStore.UI/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.UI.Data;
+
+namespace WebStore.UI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public CategoryController(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+        
+        //GET
+        public async Task<IActionResult> Index()
+        {
+            return View(await _applicationDbContext.Category.ToListAsync());
+        }
+    }
+}
