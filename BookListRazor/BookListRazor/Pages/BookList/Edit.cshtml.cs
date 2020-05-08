5e47ff6b8b8204c78fc3b96a6c5454e73b1da739
Roman Yavnikov <45608740+Romazes@users.noreply.github.com>
2020-05-08 16:08:56 +0300

    3.14. Edit Book Get Handler

---
+++ BookListRazor/BookListRazor/Pages/BookList/Edit.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class EditModel : PageModel
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public EditModel(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+        public async Task OnGet(int id)
+        {
+            Book = await _appDbContext.Book.FindAsync(id);
+        }
+    }
+}
