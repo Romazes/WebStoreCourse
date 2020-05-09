59aa8da69db7d6a3cc294a7de65d0b3032584f6c
Roman Yavnikov <45608740+Romazes@users.noreply.github.com>
2020-05-09 14:16:32 +0300

    6.6. SubCategory Create GET Action

--- WebStore/WebStore.UI/Areas/Admin/Controllers/SubCategoryController.cs
+++ WebStore/WebStore.UI/Areas/Admin/Controllers/SubCategoryController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebStore.UI.Data;
+using WebStore.UI.Models.ViewModels;
 
 namespace WebStore.UI.Areas.Admin.Controllers
 {
@@ -24,6 +25,20 @@ namespace WebStore.UI.Areas.Admin.Controllers
             var subCategories = await _applicationDbContext.SubCategory
                                                            .Include(c => c.Category).ToListAsync();
             return View(subCategories);
+        }
+
+        //GET - CREATE
+        public async Task<IActionResult> Create()
+        {
+            SubCategoryAndCategoryViewModel model = new SubCategoryAndCategoryViewModel()
+            {
+                CategoryList = await _applicationDbContext.Category.ToListAsync(),
+                SubCategory = new Models.SubCategory(),
+                SubCategoryList = await _applicationDbContext.SubCategory.OrderBy(n => n.Name)
+                    .Select(n => n.Name).Distinct().ToListAsync()
+            };
+
+            return View(model);
         }
     }
 }
