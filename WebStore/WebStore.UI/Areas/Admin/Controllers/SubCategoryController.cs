414abde964e4537ac58a22e77529b8f60aa0ca5c
Roman Yavnikov <45608740+Romazes@users.noreply.github.com>
2020-05-09 16:08:03 +0300

    6.13. SubCategory Edit Get Action

--- WebStore/WebStore.UI/Areas/Admin/Controllers/SubCategoryController.cs
+++ WebStore/WebStore.UI/Areas/Admin/Controllers/SubCategoryController.cs
@@ -81,15 +81,73 @@ namespace WebStore.UI.Areas.Admin.Controllers
             return View(modelVM);
         }
 
-         [ActionName("GetSubCategory")]
-         public async Task<IActionResult> GetSubCategory(int id)
+        //GET - ALL SUBCATEGORY FOR CURRENT CATEGORY
+        [ActionName("GetSubCategory")]
+        public async Task<IActionResult> GetSubCategory(int id)
         {
             List<SubCategory> subCategories = new List<SubCategory>();
 
             subCategories = await (from subCategory in _applicationDbContext.SubCategory
-                             where subCategory.CategoryId == id
-                             select subCategory).ToListAsync();
+                                   where subCategory.CategoryId == id
+                                   select subCategory).ToListAsync();
             return Json(new SelectList(subCategories, "Id", "Name"));
+        }
+
+        //GET - EDIT
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var subCategory = await _applicationDbContext.SubCategory.SingleOrDefaultAsync(i => i.Id == id);
+
+            if (subCategory == null)
+                return NotFound();
+
+            SubCategoryAndCategoryViewModel model = new SubCategoryAndCategoryViewModel()
+            {
+                CategoryList = await _applicationDbContext.Category.ToListAsync(),
+                SubCategory = subCategory,
+                SubCategoryList = await _applicationDbContext.SubCategory.OrderBy(n => n.Name)
+                    .Select(n => n.Name).Distinct().ToListAsync()
+            };
+
+            return View(model);
+        }
+
+        //POST - EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, SubCategoryAndCategoryViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var doesSubCategoryExists = _applicationDbContext.SubCategory.Include(c => c.Category)
+                    .Where(n => n.Name == model.SubCategory.Name && n.Category.Id == model.SubCategory.CategoryId);
+
+                if (doesSubCategoryExists.Count() > 0)
+                {
+                    //Error
+                    StatusMessage = "Error : Sub Category exists under " + doesSubCategoryExists.First().Category.Name
+                        + " category. Please use another name.";
+                }
+                else
+                {
+                    _applicationDbContext.SubCategory.Add(model.SubCategory);
+                    await _applicationDbContext.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            SubCategoryAndCategoryViewModel modelVM = new SubCategoryAndCategoryViewModel()
+            {
+                CategoryList = await _applicationDbContext.Category.ToListAsync(),
+                SubCategory = model.SubCategory,
+                SubCategoryList = await _applicationDbContext.SubCategory.OrderBy(n => n.Name)
+                    .Select(n => n.Name).ToListAsync(),
+                StatusMessage = StatusMessage
+            };
+
+            return View(modelVM);
         }
     }
 }
