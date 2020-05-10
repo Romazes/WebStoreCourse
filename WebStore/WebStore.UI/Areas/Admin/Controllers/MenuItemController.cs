de6b53cb2c81d4fba620908599490c92a2e6ee0f
Roman Yavnikov <45608740+Romazes@users.noreply.github.com>
2020-05-10 10:48:23 +0300

    7.3. Add MenuItem Index Action

---
+++ WebStore/WebStore.UI/Areas/Admin/Controllers/MenuItemController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using WebStore.UI.Data;
+
+namespace WebStore.UI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MenuItemController : Controller
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public MenuItemController(ApplicationDbContext applicationDbContext, IWebHostEnvironment webHostEnvironment)
+        {
+            _applicationDbContext = applicationDbContext;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var menuItems = await _applicationDbContext.MenuItem.Include(c => c.Category)
+                .Include(sc => sc.SubCategory).ToListAsync();
+            return View(menuItems);
+        }
+    }
+}
