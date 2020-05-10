64aa44b66dc0f08a406d0d3c19746722c5c024e4
Roman Yavnikov <45608740+Romazes@users.noreply.github.com>
2020-05-10 15:38:36 +0300

    8.13. MenuItem Edit POST Action Method

--- WebStore/WebStore.UI/Areas/Admin/Controllers/MenuItemController.cs
+++ WebStore/WebStore.UI/Areas/Admin/Controllers/MenuItemController.cs
@@ -118,12 +118,10 @@ namespace WebStore.UI.Areas.Admin.Controllers
 
             if (!ModelState.IsValid)
             {
+                MenuItemVM.SubCategory = await _applicationDbContext.SubCategory.Where(c => c.CategoryId == MenuItemVM.MenuItem.CategoryId).ToListAsync();
                 return View(MenuItemVM);
             }
-
-            _applicationDbContext.MenuItem.Add(MenuItemVM.MenuItem);
-            await _applicationDbContext.SaveChangesAsync();
-
+           
             //Work on the image saving section
             string webRootPath = _webHostEnvironment.WebRootPath;
             var files = HttpContext.Request.Form.Files;
@@ -132,23 +130,32 @@ namespace WebStore.UI.Areas.Admin.Controllers
 
             if (files.Count > 0)
             {
-                //Files has been uploaded
+                //New Image has been uploaded
                 var uploads = Path.Combine(webRootPath, "images");
-                var extension = Path.GetExtension(files[0].FileName);
+                var extension_new = Path.GetExtension(files[0].FileName);
 
-                using (var filesStream = new FileStream(Path.Combine(uploads, MenuItemVM.MenuItem.Id + extension), FileMode.Create))
+                //Delete the original file
+                var imagePath = Path.Combine(webRootPath, menuItemFromDb.PictureUri.TrimStart('\\'));
+
+                if(System.IO.File.Exists(imagePath))
+                {
+                    System.IO.File.Delete(imagePath);
+                }
+
+                //We'll upload the new file
+                using (var filesStream = new FileStream(Path.Combine(uploads, MenuItemVM.MenuItem.Id + extension_new), FileMode.Create))
                 {
                     files[0].CopyTo(filesStream);
                 }
-                menuItemFromDb.PictureUri = @"\images\" + MenuItemVM.MenuItem.Id + extension;
+                menuItemFromDb.PictureUri = @"\images\" + MenuItemVM.MenuItem.Id + extension_new;
             }
-            else
-            {
-                //No file was uploaded, so use default.
-                var uploads = Path.Combine(webRootPath, @"images\" + Utility.StaticDetail.DefaultTeaPictureUri);
-                System.IO.File.Copy(uploads, webRootPath + @"\images\" + MenuItemVM.MenuItem.Id + ".png");
-                menuItemFromDb.PictureUri = @"\images\" + MenuItemVM.MenuItem.Id + ".png";
-            }
+
+            menuItemFromDb.Name = MenuItemVM.MenuItem.Name;
+            menuItemFromDb.Description = MenuItemVM.MenuItem.Description;
+            menuItemFromDb.Price = MenuItemVM.MenuItem.Price;
+            menuItemFromDb.KindOfFlavour = MenuItemVM.MenuItem.KindOfFlavour;
+            menuItemFromDb.CategoryId = MenuItemVM.MenuItem.CategoryId;
+            menuItemFromDb.SubCategoryId = MenuItemVM.MenuItem.SubCategoryId;
 
             await _applicationDbContext.SaveChangesAsync();
 
