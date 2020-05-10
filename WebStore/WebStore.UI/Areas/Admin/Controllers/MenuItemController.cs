6c7e40094efd5025eb012ec7c313665b212d38fa
Roman Yavnikov <45608740+Romazes@users.noreply.github.com>
2020-05-10 14:59:41 +0300

    8.11. MenuItem Edit GET Action

--- WebStore/WebStore.UI/Areas/Admin/Controllers/MenuItemController.cs
+++ WebStore/WebStore.UI/Areas/Admin/Controllers/MenuItemController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using WebStore.UI.Data;
 using WebStore.UI.Models.ViewModels;
@@ -49,6 +50,71 @@ namespace WebStore.UI.Areas.Admin.Controllers
         {
             MenuItemVM.MenuItem.SubCategoryId = Convert.ToInt32(Request.Form["SubCategoryId"].ToString());
             //MenuItemVM.MenuItem.SubCategoryId = int.TryParse(Request.Form["SubCategoryId"].ToString(), MenuItemVM.MenuItem.SubCategoryId);
+
+            if (!ModelState.IsValid)
+            {
+                return View(MenuItemVM);
+            }
+
+            _applicationDbContext.MenuItem.Add(MenuItemVM.MenuItem);
+            await _applicationDbContext.SaveChangesAsync();
+
+            //Work on the image saving section
+            string webRootPath = _webHostEnvironment.WebRootPath;
+            var files = HttpContext.Request.Form.Files;
+
+            var menuItemFromDb = await _applicationDbContext.MenuItem.FindAsync(MenuItemVM.MenuItem.Id);
+
+            if (files.Count > 0)
+            {
+                //Files has been uploaded
+                var uploads = Path.Combine(webRootPath, "images");
+                var extension = Path.GetExtension(files[0].FileName);
+
+                using (var filesStream = new FileStream(Path.Combine(uploads, MenuItemVM.MenuItem.Id + extension), FileMode.Create))
+                {
+                    files[0].CopyTo(filesStream);
+                }
+                menuItemFromDb.PictureUri = @"\images\" + MenuItemVM.MenuItem.Id + extension;
+            }
+            else
+            {
+                //No file was uploaded, so use default.
+                var uploads = Path.Combine(webRootPath, @"images\" + Utility.StaticDetail.DefaultTeaPictureUri);
+                System.IO.File.Copy(uploads, webRootPath + @"\images\" + MenuItemVM.MenuItem.Id + ".png");
+                menuItemFromDb.PictureUri = @"\images\" + MenuItemVM.MenuItem.Id + ".png";
+            }
+
+            await _applicationDbContext.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        //GET - EDIT
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            MenuItemVM.MenuItem = await _applicationDbContext.MenuItem.Include(c => c.Category)
+                .Include(sc => sc.SubCategory).SingleOrDefaultAsync(i => i.Id == id);
+            MenuItemVM.SubCategory = await _applicationDbContext.SubCategory.Where(c => c.CategoryId == MenuItemVM.MenuItem.CategoryId).ToListAsync();
+
+            if (MenuItemVM.MenuItem == null)
+                return NotFound();
+
+            return View(MenuItemVM);
+        }
+
+        //POST - EDIT
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditConfirmed(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            MenuItemVM.MenuItem.SubCategoryId = Convert.ToInt32(Request.Form["SubCategoryId"].ToString());
 
             if (!ModelState.IsValid)
             {
