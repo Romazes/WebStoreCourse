d1b10ea134d80884b5eedd7cf322f7e3af85c4f3
Roman Yavnikov <45608740+Romazes@users.noreply.github.com>
2020-05-10 23:17:20 +0300

    8.3. Coupon Index View and Actions

---
+++ WebStore/WebStore.UI/Areas/Admin/Controllers/CouponController.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebStore.UI.Data;
+
+namespace WebStore.UI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CouponController : Controller
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public CouponController(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _applicationDbContext.Coupon.ToListAsync());
+        }
+    }
+}
