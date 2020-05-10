443e9effc8f9267652f85ce3f6f1542c86f1f559
Roman Yavnikov <45608740+Romazes@users.noreply.github.com>
2020-05-10 11:23:22 +0300

    7.5. MeniItem View Model and Create GET

--- WebStore/WebStore.UI/Areas/Admin/Controllers/MenuItemController.cs
+++ WebStore/WebStore.UI/Areas/Admin/Controllers/MenuItemController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 using WebStore.UI.Data;
+using WebStore.UI.Models.ViewModels;
 
 namespace WebStore.UI.Areas.Admin.Controllers
 {
@@ -12,10 +13,18 @@ namespace WebStore.UI.Areas.Admin.Controllers
         private readonly ApplicationDbContext _applicationDbContext;
         private readonly IWebHostEnvironment _webHostEnvironment;
 
+        [BindProperty]
+        public MenuItemViewModel MenuItemVM { get; set; }
+
         public MenuItemController(ApplicationDbContext applicationDbContext, IWebHostEnvironment webHostEnvironment)
         {
             _applicationDbContext = applicationDbContext;
             _webHostEnvironment = webHostEnvironment;
+            MenuItemVM = new MenuItemViewModel()
+            {
+                Category = _applicationDbContext.Category,
+                MenuItem = new Models.MenuItem()
+            };
         }
 
         public async Task<IActionResult> Index()
@@ -23,6 +32,12 @@ namespace WebStore.UI.Areas.Admin.Controllers
             var menuItems = await _applicationDbContext.MenuItem.Include(c => c.Category)
                 .Include(sc => sc.SubCategory).ToListAsync();
             return View(menuItems);
+        }
+
+        //GET - CREATE
+        public IActionResult Create()
+        {
+            return View(MenuItemVM);
         }
     }
 }
