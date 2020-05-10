34de8161d03c2a28283b6098621ef9718972b40a
Roman Yavnikov <45608740+Romazes@users.noreply.github.com>
2020-05-10 23:46:44 +0300

    8.5. Coupon Create POST Action Method

--- WebStore/WebStore.UI/Areas/Admin/Controllers/CouponController.cs
+++ WebStore/WebStore.UI/Areas/Admin/Controllers/CouponController.cs
@@ -1,7 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System.IO;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebStore.UI.Data;
+using WebStore.UI.Models;
 
 namespace WebStore.UI.Areas.Admin.Controllers
 {
@@ -10,9 +12,13 @@ namespace WebStore.UI.Areas.Admin.Controllers
     {
         private readonly ApplicationDbContext _applicationDbContext;
 
+        [BindProperty]
+        public Coupon Coupon { get; set; }
+
         public CouponController(ApplicationDbContext applicationDbContext)
         {
             _applicationDbContext = applicationDbContext;
+            Coupon = new Coupon();
         }
 
         public async Task<IActionResult> Index()
@@ -24,6 +30,34 @@ namespace WebStore.UI.Areas.Admin.Controllers
         public IActionResult Create()
         {
             return View();
+        }
+
+        //POST - CREATE
+        [HttpPost, ActionName("Create")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateConfirmed()
+        {
+            if (ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                if (files.Count > 0)
+                {
+                    byte[] picture1 = null;
+                    using (var fileStream1 = files[0].OpenReadStream())
+                    {
+                        using (var memoryStream1 = new MemoryStream())
+                        {
+                            fileStream1.CopyTo(memoryStream1);
+                            picture1 = memoryStream1.ToArray();
+                        }
+                    }
+                    Coupon.Picture = picture1;
+                }
+                _applicationDbContext.Coupon.Add(Coupon);
+                await _applicationDbContext.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(Coupon);
         }
     }
 }
