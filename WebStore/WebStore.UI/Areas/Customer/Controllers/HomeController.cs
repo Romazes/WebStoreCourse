ede42747857c077872d2dad71b135bd8a6c0788d
Roman Yavnikov <45608740+Romazes@users.noreply.github.com>
2020-05-11 15:28:55 +0300

    9.3. Home Index Action

--- WebStore/WebStore.UI/Areas/Customer/Controllers/HomeController.cs
+++ WebStore/WebStore.UI/Areas/Customer/Controllers/HomeController.cs
@@ -1,27 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using WebStore.UI.Data;
 using WebStore.UI.Models;
+using WebStore.UI.Models.ViewModels;
 
 namespace WebStore.UI.Controllers
 {
     [Area("Customer")]
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _applicationDbContext;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationDbContext applicationDbContext,ILogger<HomeController> logger)
         {
+            _applicationDbContext = applicationDbContext;
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            IndexViewModel indexVM = new IndexViewModel()
+            {
+                MenuItem = await _applicationDbContext.MenuItem.Include(c => c.Category).Include(sc => sc.SubCategory).ToListAsync(),
+                Category = await _applicationDbContext.Category.ToListAsync(),
+                Coupon = await _applicationDbContext.Coupon.Where(ca => ca.IsActive == true).ToListAsync()
+            };
+            return View(indexVM);
         }
 
         public IActionResult Privacy()
