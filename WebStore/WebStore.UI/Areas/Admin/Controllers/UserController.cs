5b3d19747d5ed07aa32a23e9021b4d5d283001d7
Roman Yavnikov <45608740+Romazes@users.noreply.github.com>
2020-05-13 02:31:51 +0300

    10.12. Lock and Unlock Users

--- WebStore/WebStore.UI/Areas/Admin/Controllers/UserController.cs
+++ WebStore/WebStore.UI/Areas/Admin/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -23,6 +24,40 @@ namespace WebStore.UI.Areas.Admin.Controllers
             var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
 
             return View(await _applicationDbContext.ApplicationUser.Where(i => i.Id != claim.Value).ToListAsync());
+        }
+
+        public async Task<IActionResult> Lock(string id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var applicationUser = await _applicationDbContext.ApplicationUser.FirstOrDefaultAsync(i => i.Id == id);
+
+            if (applicationUser == null)
+                return NotFound();
+
+            applicationUser.LockoutEnd = DateTime.Now.AddYears(100);
+
+            await _applicationDbContext.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> UnLock(string id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var applicationUser = await _applicationDbContext.ApplicationUser.FirstOrDefaultAsync(i => i.Id == id);
+
+            if (applicationUser == null)
+                return NotFound();
+
+            applicationUser.LockoutEnd = DateTime.Now;
+
+            await _applicationDbContext.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
