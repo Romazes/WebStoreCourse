a0bf5697982c19b192e7aa81065d38543eff6f5b
Roman Yavnikov <45608740+Romazes@users.noreply.github.com>
2020-05-13 00:23:03 +0300

    10.8. Users Controller

---
+++ WebStore/WebStore.UI/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using WebStore.UI.Data;
+
+namespace WebStore.UI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public UserController(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            return View(await _applicationDbContext.ApplicationUser.Where(i => i.Id != claim.Value).ToListAsync());
+        }
+    }
+}
