2b09f22f79814e374b282abe1717d704fd871202
Roman Yavnikov <45608740+Romazes@users.noreply.github.com>
2020-05-14 23:34:05 +0300

    12.4. Order Details Cart View Model

---
+++ WebStore/WebStore.UI/Models/ViewModels/OrderDetailsCart.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace WebStore.UI.Models.ViewModels
+{
+    public class OrderDetailsCart
+    {
+        public List<ShoppingCart> ListCart { get; set; }
+        public OrderHeader OrderHeader { get; set; }
+    }
+}
