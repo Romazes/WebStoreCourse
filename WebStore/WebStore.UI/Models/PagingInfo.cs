a0ebde57933e9123ec70a0f31624652803759143
Roman Yavnikov <45608740+Romazes@users.noreply.github.com>
2020-06-05 20:22:38 +0300

    13.11. PagingInfo Model

---
+++ WebStore/WebStore.UI/Models/PagingInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WebStore.UI.Models
+{
+    public class PagingInfo
+    {
+        public int TotalItem { get; set; }
+
+        public int ItemsPerPage { get; set; }
+
+        public int CurrentPage { get; set; }
+
+        public int totalPage => (int)Math.Ceiling((decimal)TotalItem / ItemsPerPage);
+
+        public string UrlParam { get; set; }
+    }
+}
