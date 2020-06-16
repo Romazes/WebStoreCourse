3c7daf8709f8aaec57bc7d5ee2cb414971ddfe10
Roman Yavnikov <45608740+Romazes@users.noreply.github.com>
2020-06-16 21:37:47 +0300

    15.7. View Component Action Method

---
+++ WebStore/WebStore.UI/ViewComponents/UserNameViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using WebStore.UI.Data;
+
+namespace WebStore.UI.ViewComponents
+{
+    public class UserNameViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _aplicationDbContext;
+
+        public UserNameViewComponent(ApplicationDbContext applicationDbContext)
+        {
+            _aplicationDbContext = applicationDbContext;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claims = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            var userFromDb = await _aplicationDbContext.ApplicationUser.FirstOrDefaultAsync(u => u.Id == claims.Value);
+
+            return View(userFromDb);
+        }
+    }
+}
