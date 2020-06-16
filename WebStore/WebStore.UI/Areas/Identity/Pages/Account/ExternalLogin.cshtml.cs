6e2dee71af78f73925b72392a0af8593a1c75f1e
Roman Yavnikov <45608740+Romazes@users.noreply.github.com>
2020-06-16 18:12:36 +0300

    15.4. Facebook Registration Page

--- WebStore/WebStore.UI/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
+++ WebStore/WebStore.UI/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
@@ -23,17 +23,20 @@ namespace WebStore.UI.Areas.Identity.Pages.Account
         private readonly UserManager<IdentityUser> _userManager;
         private readonly IEmailSender _emailSender;
         private readonly ILogger<ExternalLoginModel> _logger;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
         public ExternalLoginModel(
             SignInManager<IdentityUser> signInManager,
             UserManager<IdentityUser> userManager,
             ILogger<ExternalLoginModel> logger,
-            IEmailSender emailSender)
+            IEmailSender emailSender,
+            RoleManager<IdentityRole> roleManager)
         {
             _signInManager = signInManager;
             _userManager = userManager;
             _logger = logger;
             _emailSender = emailSender;
+            _roleManager = roleManager;
         }
 
         [BindProperty]
@@ -51,6 +54,15 @@ namespace WebStore.UI.Areas.Identity.Pages.Account
             [Required]
             [EmailAddress]
             public string Email { get; set; }
+
+            [Required]
+            public string Name { get; set; }
+
+            [Phone]
+            [Display(Name = "Phone number")]
+            public string PhoneNumber { get; set; }
+
+            public string City { get; set; }
         }
 
         public IActionResult OnGetAsync()
@@ -101,7 +113,8 @@ namespace WebStore.UI.Areas.Identity.Pages.Account
                 {
                     Input = new InputModel
                     {
-                        Email = info.Principal.FindFirstValue(ClaimTypes.Email)
+                        Email = info.Principal.FindFirstValue(ClaimTypes.Email),
+                        Name = info.Principal.FindFirstValue(ClaimTypes.Name)
                     };
                 }
                 return Page();
